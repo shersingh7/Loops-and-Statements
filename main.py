8d18d581409b5f22ed49cf34bf9c9b81045222ac
Davinder <66485246+shersingh7@users.noreply.github.com>
2021-10-30 09:13:39 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,87 @@
+## If else statements
+
+# temp = input("Whats the temp? ")
+#
+# if(int(temp) > 20):
+#     print("Enjoy the nice day")
+# if(int(temp) < 10):
+#     print("Use warm clothes, it's cold out there")
+
+## Exercise
+
+# price = 1
+# good_credit = True
+#
+# if good_credit:
+#     print("The down payment is " + str(1*0.1))
+# else:
+#     print("The down payemnt is " + str(1*0.2))
+
+## AND / OR
+
+# has_high_income = True
+# has_good_credit = True
+#
+# if has_high_income and has_good_credit:
+#     print("Good boy")
+# else:
+#     print("Come on bruh")
+
+
+# has_high_income = True
+# has_good_credit = True
+#
+# if has_high_income or has_good_credit:
+#     print("Good boy")
+# else:
+#     print("Come on bruh")
+
+## Excersie
+
+# name = input("Please enter the name: ")
+#
+# if len(name) < 3:
+#     print("The name is too short")
+# elif len(name) > 50:
+#     print("The name is too big")
+# else:
+#     print("Name looks good!")
+
+
+# weight = input("Enter the Weight: ")
+# unit = input("Unit (L / K): ")
+#
+# if unit == "L":
+#     print(int(weight) / 0.45)
+# else:
+#     print(int(weight) * 0.45)
+
+
+## For loops
+
+# for item in 'Python':
+#     print(item)
+#
+# for item in ['Davinder','Jay','Pratik','Sahil','Don']:
+#     print(item)
+#
+# for item in range(10):
+#     print(item)
+
+
+##Exercise
+# prices = [10,20,30]
+# total = 0
+#
+# for item in prices:
+#     total += item
+# print(total)
+
+
+## Nested Loops
+
+
+# for i in range(1 , 11):
+#     for j in range(1,11):
+#         print(f'{i} * {j} = {i*j}')
+
